ee2e21d68a7009d1737fdc2cc4bcadd7a03f4a35
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-10-19 00:18:50 +0200

    Button class and function App() added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './Components/Button';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [firstNumber, setFirstNumber] = useState(1);
+
+    const increment = () => {
+        if (firstNumber >= 0 && firstNumber !== 16) {
+            setFirstNumber(firstNumber + 1);
+        }
+    }
+
+    const decrement = () => {
+        if (firstNumber > 0 && firstNumber <= 16) {
+            setFirstNumber(firstNumber - 1);
+        }
+    }
+
+    return (
+        <>
+            <Button classButton="button" text="<<" actionOnClick={decrement} />
+            <Button classButton="number button" text={firstNumber} />
+            <Button classButton="number button" text={firstNumber + 1} />
+            <Button classButton="number button" text={firstNumber + 2} />
+            <Button classButton="number button" text={firstNumber + 3} />
+            <Button classButton="number button" text={firstNumber + 4} />
+            <Button classButton="button" text=">>" actionOnClick={increment} />
+        </>
+    );
 }
 
 export default App;
